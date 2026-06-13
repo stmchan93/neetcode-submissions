a98f2c485968ef1d40b83bcf3babe38d3a3a2f28
stmchan93 <51380425+stmchan93@users.noreply.github.com>
2026-06-13 16:48:06 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    // i would have two pointers start at start + end of string
+    // i would just see if they equal eahc ohter and the moment they don't its not a palindrome anymore
+    isPalindrome(s) {
+        s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+        let start = 0;
+        let end = s.length - 1;
+        console.log("S: ", s);
+        for(let i = 0; i < s.length; i++) {
+            if (start === end || start > end) {
+                return true;
+            } else if (s[start] !== s[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
