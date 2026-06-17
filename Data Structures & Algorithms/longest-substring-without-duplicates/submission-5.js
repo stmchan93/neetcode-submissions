13768a66de63efeb6476e7468cfd711b48d47967
stmchan93 <51380425+stmchan93@users.noreply.github.com>
2026-06-17 13:43:41 -0700

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        if (s.length === 0 || s.length === 1) {
+            return s.length;
+        }
+        let left = 0;
+        const set = new Set();
+        let result = 0;
+        // two pointer soultion where i have a left and right pointer
+        // move the right pointer until i see a character that i've seen before
+        // if i've seen the character before then i move the left one over
+
+        // now how do i check if i've seen a character before? i guess a map of the char to the index position
+        // two-pointer solution, start with the left, go right, and find if the set contains the sliding window!
+        for(let right = 0; right < s.length; right++) {
+            while (set.has(s[right])) {
+                set.delete(s[left]);
+                left++;
+            }
+            set.add(s[right]);
+            result = Math.max(result, right - left + 1);
+        }
+        return result;
+    }
+}
