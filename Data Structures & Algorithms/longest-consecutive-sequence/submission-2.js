ff2834ddbaf3ba0bf05b4b1f15096faeb5b2a7e4
stmchan93 <51380425+stmchan93@users.noreply.github.com>
2026-06-25 12:53:02 -0700

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    
+    // brute force is to sort the array and count from some number and to see if that number is +1 of the
+    // previous number. the problem with this ist hat its O(nlogn + n) which is unacceptable
+    // the better solution is to store the count of the element in a map and just to keep seeing if the next
+    // number + 1 of it is in the map, if it is then we increment our output. if it's not, we reset our 
+    // output to 0
+    // first we need to loop through and store the map first so thats O(n)
+    // next we need to loop through the araray again adn see if hte number is in the array
+    longestConsecutive(nums) {
+        const set = new Set();
+        for(let i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        let res = 0;
+        for(let i = 0; i < nums.length; i++) {
+           if (set.has(nums[i] - 1)) {
+                continue;
+           }
+           let currNum = nums[i];
+            let currMax = 0;
+           while (set.has(currNum)) {
+                currNum++;
+                currMax++;
+                res = Math.max(currMax, res);
+           }
+        }
+        return res;
+    }
+}
